9985e237f8787d1f12f1fbb3677e14708a8663d8
rmogusu <rmogusu123@gmail>
2020-05-19 12:48:17 +0200

    Add required depencies for local testing of my app.

--- app/src/main/java/com/moringaschool/petfinder/PetsActivity.java
+++ app/src/main/java/com/moringaschool/petfinder/PetsActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PetsActivity extends AppCompatActivity {
-
+    private TextView mLocationTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pets);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the pets near: " + location);
     }
 }
