2c63765f8dff61d6a51fb9dbfad94d55cb936442
rmogusu <rmogusu123@gmail>
2020-05-19 10:31:22 +0200

    Add new activity to be able to navigate between the activity.

--- app/src/main/java/com/moringaschool/petfinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/petfinder/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.petfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         mFindPetsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PetsActivity.class);
+                startActivity(intent);
                 Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
         });
