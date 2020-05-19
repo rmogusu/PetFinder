0e0e0f284e7c4cea2498e8f3a08ac9d916e6d924
rmogusu <rmogusu123@gmail>
2020-05-19 10:21:21 +0200

    Add a toast inside onclick listener.

--- app/src/main/java/com/moringaschool/petfinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/petfinder/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindPetsButton;
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         mFindPetsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
         });
     }
