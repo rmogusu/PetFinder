02952e3573155b9d1317e5a7873f2f58241fbf73
rmogusu <rmogusu123@gmail>
2020-05-19 11:03:55 +0200

    Add a form field to take in the specified location.

--- app/src/main/java/com/moringaschool/petfinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/petfinder/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, PetsActivity.class);
                 startActivity(intent);
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
             }
         });
     }
