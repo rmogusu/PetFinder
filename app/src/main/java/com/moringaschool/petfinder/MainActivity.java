49dc30024bbaa84f87e87c29e856e35b06b25df0
rmogusu <rmogusu123@gmail>
2020-05-19 10:12:59 +0200

    Add an onclick listener to listen to the button click.

--- app/src/main/java/com/moringaschool/petfinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/petfinder/MainActivity.java
@@ -3,12 +3,21 @@ package com.moringaschool.petfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mFindPetsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindPetsButton = (Button)findViewById(R.id.findPetsButton);
+        mFindPetsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do something
+            }
+        });
     }
 }
