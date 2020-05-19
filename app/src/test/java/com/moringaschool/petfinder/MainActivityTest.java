fca2424ecd904a2873587c18432f01d9fb40f00b
rmogusu <rmogusu123@gmail>
2020-05-19 13:29:15 +0200

    Add test to assert that text in petfinder is equal to test in appNameTextView and make sure they pass.

--- app/src/test/java/com/moringaschool/petfinder/MainActivityTest.java
+++ app/src/test/java/com/moringaschool/petfinder/MainActivityTest.java
@@ -1,14 +1,25 @@
 package com.moringaschool.petfinder;
 
+import android.widget.TextView;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
+import static junit.framework.TestCase.assertTrue;
+
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
+    private MainActivity activity;
     @Before
     public void setup(){
         activity = Robolectric.setupActivity(MainActivity.class);
+    }
+    @Test
+    public void validateTextViewContent(){
+        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+        assertTrue("PetFinder".equals(appNameTextView.getText().toString()));
     }
 }
